26ee4d7f1daec6163989ac5bb15af6554b5dc388
kcrazy <thekcrazy@gmail.com>
2021-10-26 17:31:33 +0800

    WIP

--- wdk/src/lib.rs
+++ wdk/src/lib.rs
@@ -26,7 +26,14 @@ fn panic(_info: &PanicInfo) -> ! {
 #[no_mangle]
 pub static _fltused: i32 = 0;
 
+#[cfg(target_arch = "x86_64")]
 #[no_mangle]
 pub extern "system" fn __CxxFrameHandler3() -> i32 {
+    0
+}
+
+#[cfg(target_arch = "x86")]
+#[link_name = "___CxxFrameHandler3@0"]
+pub extern "system" fn __cxx_frame_handler3_0() -> i32 {
     0
 }
