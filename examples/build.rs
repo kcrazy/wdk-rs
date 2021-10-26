8fc70d8ea840d395071bb5018891258fdaadf8d1
kcrazy <thekcrazy@gmail.com>
2021-10-26 15:41:24 +0800

    hello world

--- examples/build.rs
+++ examples/build.rs
@@ -1,22 +1,5 @@
-use wdk_build::{get_km_dir, DirectoryType};
+use wdk_build::{build};
 
 fn main() {
-    // Get the path to the kernel libraries.
-    let dir = get_km_dir(DirectoryType::Library).unwrap();
-
-    // Append the architecture based on our target.
-    let target = std::env::var("TARGET").unwrap();
-
-    let arch = if target.contains("x86_64") {
-        "x64"
-    } else if target.contains("i686") {
-        "x86"
-    } else {
-        panic!("The target {} is currently not supported.", target);
-    };
-
-    let dir = dir.join(arch);
-
-    // Specify the link path.
-    println!("cargo:rustc-link-search=native={}", dir.to_str().unwrap());
+    build().unwrap();
 }
--- examples/examples/hello-world.rs
+++ examples/examples/hello-world.rs
@@ -1,8 +1,10 @@
 #![no_std]
 
 use wdk::println;
+use wdk_sys::base::{DRIVER_OBJECT, NTSTATUS, STATUS_SUCCESS, UNICODE_STRING};
 
 #[no_mangle]
-extern "C" fn _DllMainCRTStartup() {
+pub extern "system" fn driver_entry(_driver: &mut DRIVER_OBJECT, _: &UNICODE_STRING) -> NTSTATUS {
     println!("Hello World!");
+    STATUS_SUCCESS
 }
