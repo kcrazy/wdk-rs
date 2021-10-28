432804141c4fc90a56bb542e49c206c6dce4286c
kcrazy <thekcrazy@gmail.com>
2021-10-28 11:22:45 +0800

    Driver compiled to exe

--- examples/examples/hello-world.rs
+++ examples/examples/hello-world.rs
@@ -1,15 +1,16 @@
 #![no_std]
+#![no_main]
 
 use wdk::println;
 use wdk_sys::base::{DRIVER_OBJECT, NTSTATUS, STATUS_SUCCESS, UNICODE_STRING};
 
 #[no_mangle]
-pub extern "system" fn driver_entry(driver: &mut DRIVER_OBJECT, _: &UNICODE_STRING) -> NTSTATUS {
+extern "system" fn driver_entry(driver: &mut DRIVER_OBJECT, _: &UNICODE_STRING) -> NTSTATUS {
     driver.DriverUnload = Some(driver_exit);
     println!("Hello World!");
     STATUS_SUCCESS
 }
 
-pub extern "C" fn driver_exit(_driver: *mut DRIVER_OBJECT) {
+extern "C" fn driver_exit(_driver: *mut DRIVER_OBJECT) {
     println!("Bye bye!");
 }
